4860a5349969cda2d76899d217ac548071d096ba
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2023-11-18 11:43:09 +0100

    add notebook

--- run.py
+++ run.py
@@ -32,21 +32,8 @@ neg_labels = -1 * np.ones(len(neg_tweets), dtype=int)  # Assign label -1 for neg
 train_tweets = pos_tweets + neg_tweets
 labels = np.concatenate((pos_labels, neg_labels), axis=0)
 
-# Construct feature representations for training tweets
-def average_word_vectors(tweet, embeddings, word_to_embedding):
-    words = tweet.split()
-    vectors = [word_to_embedding[word] for word in words if word in word_to_embedding]
-    if vectors:
-        return np.mean(vectors, axis=0)
-    else:
-        # If none of the words in the tweet are in the embeddings, return a zero vector
-        return np.zeros_like(embeddings[0])
-
-# Construct feature representations for training tweets
-train_features = [average_word_vectors(tweet, word_embeddings, word_to_embedding) for tweet in train_tweets]
-
 ### TRAINING THE LINEAR CLASSIFIER
-
+# !pip install scikit-learn
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -61,7 +48,7 @@ def average_word_vectors(tweet, word_to_embedding):
         return np.mean(vectors, axis=0)
     else:
         # If none of the words in the tweet are in the embeddings, return a zero vector
-        return np.zeros_like(word_to_embedding["you"])
+        return np.zeros_like(word_embeddings[0])
 
 
 # Construct feature representations for training tweets
@@ -81,9 +68,10 @@ y_pred = model.predict(X_val)
 accuracy = accuracy_score(y_val, y_pred)
 print(f"Validation Accuracy: {accuracy}")
 
-
 # Construct feature representations for test tweets
-test_features = [average_word_vectors(tweet, word_embeddings) for tweet in test_tweets]
+import csv
+test_data_path = 'twitter-datasets/test_data.txt'
+test_features = [average_word_vectors(tweet, word_to_embedding) for tweet in test_tweets]
 
 # Make predictions
 y_test_pred = model.predict(test_features)
@@ -103,16 +91,16 @@ def create_csv_submission(ids, y_pred, name):
             writer.writerow({"Id": int(r1), "Prediction": int(r2)})
 
 def get_test_ids(path):
-	file = open(path,'r')
-	lines = file.readlines()
-	for rowidx in range(len(lines)):
-		index = lines[rowidx].index(',')
-		lines[rowidx] = lines[rowidx][:index]
-	return lines
+    file = open(path,'r')
+    lines = file.readlines()
+    for rowidx in range(len(lines)):
+        index = lines[rowidx].index(',')
+        lines[rowidx] = lines[rowidx][:index]
+    return lines
 
 ids_test = get_test_ids(test_data_path)
 y_pred = []
 y_pred = y_test_pred
 y_pred[y_pred <= 0] = -1
 y_pred[y_pred > 0] = 1
-create_csv_submission(ids_test, y_pred, "submission")
+create_csv_submission(ids_test, y_pred, "submission.csv")
