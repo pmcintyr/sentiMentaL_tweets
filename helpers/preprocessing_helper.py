ac93800e03439b2442d1f1e407e6e5c300751854
Simon Li <simonjunyou.li@gmail.com>
2023-12-06 01:54:17 +0100

    clean preprocessing

--- helpers/preprocessing_helper.py
+++ helpers/preprocessing_helper.py
@@ -5,16 +5,16 @@ import pandas as pd
 import nltk
 import ssl
 import sys
+import multiprocessing
+import subprocess
 
+from symspellpy import SymSpell
+from collections import deque
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 from nltk.stem import PorterStemmer
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
-
-import subprocess
-import sys
-from symspellpy import SymSpell
 
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.download('stopwords')
@@ -53,22 +53,18 @@ def get_wordnet_tag(nltk_tag):
     else:
         return wordnet.NOUN
     
-def drop_duplicates(data):
+def drop_duplicates():
     data = data.drop_duplicates(subset=['text'])
-    return data
     
-def remove_elongs(data):
+def remove_elongs():
     data['text'] = data['text'].apply(
       lambda text: str(re.sub(r'\b(\S*?)(.)\2{3,}\b', r'\1\2\2\2', text)))
-    return data
 
-def lower_case(data):
+def lower_case():
     data['text'] = data['text'].str.lower()
-    return data
 
-def spell_correct(data):
+def spell_correct():
     data['text'] = data['text'].apply(lambda text: symspell.lookup_compound(text, max_edit_distance=2)[0].term)
-    return data
 
 def lemmatize(text):
     nltk_tagged = nltk.pos_tag(text.split())
@@ -78,30 +74,20 @@ def lemmatize(text):
       [lemmatizer.lemmatize(w, get_wordnet_tag(nltk_tag))
        for w, nltk_tag in nltk_tagged])
 
-def lemmatizer(data):
+def lemmatizer():
     data['text'] = data['text'].apply(lemmatize)
-    return data
 
-def stopword(data):
+def stopword():
     stopwords_ = set(stopwords.words('english'))
 
     data['text'] = data['text'].apply(
       lambda text: ' '.join(
         [word for word in str(text).split() if word not in stopwords_]))
     
-    return data
-    
-def hashtag(data):
+def hashtag():
     data['text'] = data['text'].apply(
       lambda text: str(re.sub(r'[\<].*?[\>]', '', text)))
     data['text'] = data['text'].apply(lambda text: text.strip())
-    
-    return data
-
-import re
-import multiprocessing
-
-from collections import deque
 
 def split_hashtag(text, token='#', vocabulary=set()):
     # Find the word(s) following the token
@@ -115,7 +101,6 @@ def split_hashtag(text, token='#', vocabulary=set()):
     full_hashtag = token + hashtag
     if full_hashtag in vocabulary:
         return [full_hashtag]
-    
     
     words = []
     remaining = deque(hashtag)
@@ -220,84 +205,9 @@ def spacing():
     data['text'] = data['text'].apply(lambda text: text.strip())
     data.reset_index(inplace=True, drop=True)
 
-def nltk_resource():
-    # Download the stopwords resource
-    ssl._create_default_https_context = ssl._create_unverified_context
-    nltk.download('stopwords')
-
-def preprocess_tweet_old(tweet):
-    # Convert to lowercase
-    tweet = tweet.lower()
-    
-    # Remove URLs
-    tweet = re.sub(r'http\S+|www\S+|https\S+', '', tweet, flags=re.MULTILINE)
-    
-    # Remove mentions
-    tweet = re.sub(r'@[A-Za-z0-9]+', '', tweet)
-    
-    # Remove hashtags
-    tweet = re.sub(r'#', '', tweet)
-    
-    # Tokenization using NLTK's TweetTokenizer
-    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
-    tokens = tokenizer.tokenize(tweet)
-    
-    # Remove punctuation and numbers
-    tokens = [word for word in tokens if word.isalpha()]
-    
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
-    
-    # Stemming using NLTK's PorterStemmer
-    stemmer = PorterStemmer()
-    tokens = [stemmer.stem(word) for word in tokens]
-    
-    # Reassemble the tokens into a processed tweet
-    processed_tweet = ' '.join(tokens)
-    
-    return processed_tweet
-
-# Example usage
-# tweet = "Great tutorial on #SentimentAnalysis using NLTK! @username http://example.com #NLP"
-# processed_tweet = preprocess_tweet(tweet)
-# print("Original tweet:", tweet)
-# print("Processed tweet:", processed_tweet)
-
-# Load positive training tweets and assign labels
-with open('../twitter-datasets/train_pos.txt', 'r', encoding='utf-8') as file:
-    pos_tweets = file.readlines()
-
-with open('../twitter-datasets/train_neg.txt', 'r', encoding='utf-8') as file:
-    neg_tweets = file.readlines()
-
-with open('../twitter-datasets/test_data.txt', 'r', encoding='utf-8') as file:
-    test_tweets = file.readlines()
-
-
-processed_pos_tweets, processed_neg_tweets, processed_test_tweets = [], [], []
-
-for pos_tweet, neg_tweet, test_tweet in zip(pos_tweets, neg_tweets, test_tweets):
-    processed_pos_tweets.append(preprocess_tweet_old(pos_tweet))
-    processed_neg_tweets.append(preprocess_tweet_old(neg_tweet))
-    processed_test_tweets.append(preprocess_tweet_old(test_tweet))
-
-# save to files
-pos_file_path = '../twitter-datasets/processed_train_pos.txt'
-neg_file_path = '../twitter-datasets/processed_train_neg.txt'
-test_file_path = '../twitter-datasets/processed_test_data.txt'
-
-with open(pos_file_path, 'w', encoding='utf-8') as file:
-    for tweet in processed_pos_tweets:
-        file.write(tweet + '\n')
-
-with open(neg_file_path, 'w', encoding='utf-8') as file:
-    for tweet in processed_neg_tweets:
-        file.write(tweet + '\n')
-
-with open(test_file_path, 'w', encoding='utf-8') as file:
-    for tweet in processed_test_tweets:
-        file.write(tweet + '\n')
+def final_parenthesis(self):
+    data['text'] = data['text'].str.replace('\)+$', ':)')
+    data['text'] = data['text'].str.replace('\(+$', ':(')
 
 def main(argv):
     dataset = argv[0]
@@ -333,15 +243,14 @@ def main(argv):
                 data = pd.concat([data, df], ignore_index=True)
         
     if dataset == 'train' or dataset == 'train_full':
-        data = drop_duplicates(data)
+        data = data.drop_duplicates(subset=['text'])
 
-    if model == 'distilbert':
-        data = lower_case(data)
-        data = remove_tags(data)
-        data = remove_elongs(data)
-        data = prune_punctuations(data)
-        data = spacing(data)
-        data = empty(data)
+    if model == 'distilbert' or model == 'bert':
+        lower_case()
+        remove_tags()
+        final_parenthesis()
+        spacing()
+        empty()
 
     data = data.sample(frac=1)
 
